5225d498ecf0344f2e85997b6f648343eed436ab
NareshKumar <nanaresh476@gmail.com>
2025-07-16 21:41:58 +0530

    b.1

---
+++ print_hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>     // Header for standard input/output functions
+
+int main()             // Main function - program starts here
+{
+    printf("HELLO WORLD");  // Output statement
+    return 0;               // Exit the program
+}
