4842ba865748bcfa9d40022e90b9f92fe31bc14a
NareshKumar <nanaresh476@gmail.com>
2025-07-16 21:47:33 +0530

    b.2

---
+++ Add_Two_Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 6, sum;
+
+    sum = a + b;
+
+    printf("The sum of a and b is %d", sum);          // Method 1: Using 'sum' variable
+    printf("\n the sum of a+b=%d", a + b);            // Method 2: Direct expression
+
+    return 0;
+}
